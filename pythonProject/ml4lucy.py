50bc792f71f67215a658e879b3452f3b89aef22f
JY Li <jyli@Jianyings-MacBook-Pro.local>
2020-11-04 23:21:36 -0500

    python more

--- pythonProject/ml4lucy.py
+++ pythonProject/ml4lucy.py
@@ -10,43 +10,43 @@ from matplotlib.colors import ListedColormap
 
 def main():
     data = pd.read_csv('/Users/jyli/myGit/java4lucy/data/input.txt')
-    print(data.head())
+ #   print(data.head())
    # decisionTree(data)
 
 ##  test female survival rate
     dFemale = data.copy()
     dFemale = dFemale[dFemale['gender'] == 0]
-    print (dFemale)
+ #   print (dFemale)
     surv = dFemale['Survived/Not']
-    print(type(surv))
+ #   print(type(surv))
     asn = surv.to_numpy()
-    print(asn.sum()/asn.size)
-
+ #   print(asn.sum()/asn.size)
+    print("Female survival rate  is: " + str(asn.sum()/asn.size))
 
     #print(data['gender']==0 & data['Age'] < 9)
-    print(data['Age'] < 9)
-    print(data['gender']==0 )
+   # print(data['Age'] < 9)
+   # print(data['gender']==0 )
     d2 = data.copy()
 
 
 
     d2 = d2[d2['gender']==0]
     #d2 = d2[d2['gender']==1]
-    print(d2.head())
+    #print(d2.head())
 
     d2 = d2[d2['Age']<9]
-    print(d2.head())
+    #print(d2.head())
 
     d2 = d2[d2['Sibling']<2.9]
-    print(d2.head())
+    #print(d2.head())
 
    # print(d2['Survived/Not'])
     surv = d2['Survived/Not']
-    print(type(surv))
+    #print(type(surv))
 
     asn = surv.to_numpy()
 
-    print(asn.sum()/asn.size)
+    print("proportion is: " + str(asn.sum()/asn.size))
 
 
     #data <- data[data['gender']==0]
@@ -63,7 +63,7 @@ def decisionTree ():
     #from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test =  train_test_split(X,y,test_size = 0.25, random_state= 0)
 
-    print(X_test[0:5,])
+    #print(X_test[0:5,])
     #feature scaling
 
     sc_X = StandardScaler()
@@ -80,10 +80,10 @@ def decisionTree ():
 
     y_pred = classifier.predict(X_test)
 
-
     #from sklearn import metrics
 
     print('Accuracy Score:', metrics.accuracy_score(y_test,y_pred))
+
 
 
     ##  test on the know data
@@ -91,14 +91,21 @@ def decisionTree ():
     feature_cols = ['gender',  'Age' , 'SiblingAge']
     X2 = dt2.iloc[:,[0,1,2]].values
     y2 = dt2.iloc[:,3].values
-    print(X2[0:5,])
-    X2 = sc_X.fit_transform(X2)
-    y2_pred = classifier.predict(X2)
+    #print(X2[0:5,])
 
+    X2 = sc_X.fit_transform(X2)
+    #print(X2[0:5,])
+    y2_pred = classifier.predict(X2)
     print('Test Accuracy Score:', metrics.accuracy_score(y2,y2_pred))
-   # print (y2_pred.size)
-   # print(y2.size)
-    #print(X)
+
+    ##  Train on full data set
+    X_all = sc_X.fit_transform(X)
+    classifier2 = DecisionTreeClassifier()
+    classifier2 = classifier2.fit(X_all,y)
+    y2_all_pred = classifier2.predict(X2)
+    print('Test Accuracy Score with all data:', metrics.accuracy_score(y2,y2_all_pred))
+
+
 if __name__ == '__main__':
- #   main()
+    main()
     decisionTree ()
