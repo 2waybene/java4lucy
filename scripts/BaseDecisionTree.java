d07f9380e3bb764cdb63c2b11426043426ea0563
JY Li <jyli@Jianyings-MacBook-Pro.local>
2023-02-22 17:06:32 -0500

    added more testing

--- scripts/BaseDecisionTree.java
+++ scripts/BaseDecisionTree.java
@@ -1,9 +1,12 @@
-/**
-  * Basic code for assignment 2 - F2020
-  * @author Sadeghi F, Safaa Bedawi
-*/
+﻿import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
 
-package assignment2;
+
+
+//package assignment2;
 
 public class BaseDecisionTree {
 	private int featureIndex; // feature index on which
@@ -24,10 +27,10 @@ public class BaseDecisionTree {
      */
     public BaseDecisionTree() {
 
-        this.featureIndex = -1;
+        this.featureIndex = -1; //has no more children 
         this.threshold = 0.0;
 
-        classFreq = new int[nClasses];
+        classFreq = new int[nClasses]; //class:survise or not. frequency: percentage
     }
 
     /**
@@ -48,7 +51,28 @@ public class BaseDecisionTree {
  
     public int getDecision(double[] vector) {
       // write your code here.
-       	  return 0;
+    	//if Prob1>Prob2:return 1,else return 0
+    	//vector = {dim1,dim2,dim3} gender age slibing
+    	//vector[dim];
+    	/*
+    	 * recursion
+    	 * base case: 
+    	 */
+    	if (isExternal()) {
+    		return getMaxClass();
+    	}
+    	else {
+    		if (vector[featureIndex]>threshold) {
+    			return greaterBranch.getDecision(vector);
+    		}
+    		else {
+    			return smallerBranch.getDecision(vector);
+    		}
+    	}
+    	//vector[featureIndex]
+    	
+    	
+       	  //return 0;
     }
     
     /**
@@ -56,7 +80,22 @@ public class BaseDecisionTree {
      */
     public int getMaxClass() {
     //  write your code here.
-        return 0;
+    	if (isExternal()) {  //if it is the leaf
+    		if (classFreq[0]>classFreq[1]) {
+    			return 0;
+    		}
+    		else {
+    			return 1;
+    		}
+    	}
+    	else {	//if it is node, then compare children node or leaf
+    		if (smallerBranch.getMaxProb()>greaterBranch.getMaxProb()) {
+    			return 0;//smaller branch has bigger prob
+    		}
+    		else {
+    			return 1;
+    		}
+    	}
     }
 
     /**
@@ -73,8 +112,18 @@ public class BaseDecisionTree {
     public double getMaxProb() {
    
    //    write your code here.
+    	//if it is the leaf -----isExternal()==-1
+    	//total
+    	//
+    	double result = 0;
+    	if (isExternal()) {
+    		result = Math.max(classFreq[0]/total,classFreq[1]/total);
+    	}
+    	else {
+    		result = Math.max(smallerBranch.getMaxProb(), greaterBranch.getMaxProb());
+    	}
    
-  	  return 0;
+  	  return result;
     }
     
     /**
@@ -82,7 +131,7 @@ public class BaseDecisionTree {
      * to use instead of updateProbCount
      */
     
-   public void setProb(int c, int percentage) {
+   public void setProb(int c, int percentage) { //c=0:not survive, c=1:survive
 
         classFreq[c] = percentage;
         total = 100;
@@ -96,14 +145,84 @@ public class BaseDecisionTree {
    *For example for feature 1 (male/female) we have female with 27% survived, count
    how many samples of female that has survived/total number of females.
    */
- 
+   
+   
    public int[] CalcProb(String inputFeatureWLabel) {  	
    /*
     *       write your code here.
-   */
-	int[] myList = {1, 2, 3, 4};
-	return myList;   
+   */   
+	   String input_line;
+	   String[] lineArr;
+	   double total_female = 0;
+	   double female_survive = 0;
+	   double total_age = 0;
+	   double age_survive=0;
+	   double total_sibling = 0;
+	   double sibling_survive = 0;
+	   BufferedReader reader;
+
+	   try {
+		reader = new BufferedReader(new FileReader("input.txt"));
+		input_line=reader.readLine();//get next line of input.txt
+		while ((input_line=reader.readLine())!=null) {
+			lineArr = input_line.split(",");
+			//temp_class = Integer.parseInt(lineArr[input_number]);//lineArr[dim];//which attribute(gender,age,sibling) whole coloum value
+			total++;
+
+			if (inputFeatureWLabel.equals("gender")) {		
+				if (Integer.parseInt(lineArr[0])==0) {
+					total_female++;
+					if (Integer.parseInt(lineArr[3])==1){
+						female_survive++;
+					}
+	            }
+				double value = Double.valueOf(female_survive)/Double.valueOf(total_female);
+				classFreq[1]= (int)Math.round(100*value);
+				classFreq[0]= 100-classFreq[1];
+			}
+			if (inputFeatureWLabel =="age") {
+				if (Integer.parseInt(lineArr[1])<9.9 && Integer.parseInt(lineArr[0])==1) {
+//					age_survive++;
+					total_age++;
+					if (Integer.parseInt(lineArr[3])==1) {
+						age_survive++;
+					}
+					
+				}
+
+				double age_value = Double.valueOf(age_survive)/Double.valueOf(total_age);
+				classFreq[1]= (int)Math.round(100*age_value);
+				classFreq[0]= 100-classFreq[1];
+			}
+			if (inputFeatureWLabel =="sibling") {
+				if (Integer.parseInt(lineArr[2])>2.9 && Integer.parseInt(lineArr[1])<9.9 && Integer.parseInt(lineArr[0])==1) {
+					total_sibling++;
+					if (Integer.parseInt(lineArr[3])==1) {
+						sibling_survive++;
+					}
+					
+				}
+				double sibling_value = Double.valueOf(sibling_survive)/Double.valueOf(total_sibling);
+				classFreq[1]= (int)Math.round(100*sibling_value);
+				classFreq[0]= 100-classFreq[1];
+
+			}
+		
+		}
+		
+
+	   } catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return classFreq;
+	   
+	   
     }
+   
 
 
     /*
@@ -122,6 +241,8 @@ public class BaseDecisionTree {
     /*
      *       write your code here.
      */
+    	greaterBranch = ds;
+    	ds.parent = this;
     }
 
     /*
@@ -164,10 +285,59 @@ public class BaseDecisionTree {
      */
   
     public int[] CalcAccuracy(String inputFeatureWLabel) {
-
+    	
     //   write your code here.
-	int[] myList = {1, 2, 3, 4}; 
-	return myList;
+ 	   String input_line;
+ 	   String[] lineArr;
+ 	   int line_count=0;
+ 	   double[] tempFeatureArr = new double[dim];
+ 	   int classCorrect = 0;
+ 	   //BaseDecisionTree current = null;//current node, usage: current.getDecision(tempFeatureArr)
+ 	   BufferedReader reader;
+ 	   
+ 	   try {
+ 		   	reader = new BufferedReader(new FileReader(inputFeatureWLabel));
+ 			input_line=reader.readLine();//get next line of input.txt
+ 			while ((input_line=reader.readLine())!=null) {
+ 				lineArr = input_line.split(",");
+ 				line_count++;//50
+ 				for (int i =0;i<dim;i++) {
+ 					tempFeatureArr[i] = Integer.parseInt(lineArr[i]); //save vector value
+ 				}
+ 				if (getDecision(tempFeatureArr) == Integer.parseInt(lineArr[dim])){
+ 					classCorrect++;
+ 				}
+ 			}
+ 	   } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+ 	   }
+ 	   
+ 	   
+// 	   try {
+// 		  //reader = new BufferedReader(new FileReader("output.txt"));
+// 		   	reader = new BufferedReader(new FileReader(inputFeatureWLabel));
+// 			input_line=reader.readLine();//get next line of input.txt
+// 			while ((input_line=reader.readLine())!=null) {
+// 				lineArr = input_line.split(",");
+// 				//line_count++;//50
+// 				for (int i =0;i<dim;i++) {
+// 					tempFeatureArr[i] = Integer.parseInt(lineArr[i]); //save vector value
+// 				}
+// 				
+// 				
+// 				
+// 			}
+// 	   }catch (IOException e) {
+// 			// TODO Auto-generated catch block
+// 			e.printStackTrace();
+// 		}
+		//return classFreq;
+ 	   //int[] result = new int[4];
+ 	   int[] result = new int[] {classCorrect*100/line_count};
+ 	   return result;
+ 	   
+
     }
 
 
@@ -183,8 +353,37 @@ public class BaseDecisionTree {
     
     public void print() {
 
-       // write your code here.       
- 
+       // write your code here. 
+    	//print(parent);
+    	System.out.println(toString());//print node
+        if(!isExternal()) {	//recursion
+            smallerBranch.print();
+            greaterBranch.print();
+        }   	 	
+  	
+     }
+    
+    public void print(BaseDecisionTree parent) {
+    	if (parent == null) {
+    		return;
+    	}
+    	Stack<BaseDecisionTree> stack = new Stack<BaseDecisionTree>();
+    	stack.push(parent);
+    	
+    	while(!stack.empty()) {
+    		BaseDecisionTree n =stack.pop();
+    		System.out.printf("%d",n);
+    		
+    		if (n.getGreaterBranch() != null) {
+    			stack.push(n.getGreaterBranch());
+    			print(n.getGreaterBranch());
+    		}
+    		if (n.getSmallerBranch() != null) {
+    			stack.push(n.getSmallerBranch());
+    			print(n.getSmallerBranch());
+    		}
+    		
+    	}
     }
     
     /**
--- scripts/BaseDecisionTreeTester.java
+++ scripts/BaseDecisionTreeTester.java
@@ -1,10 +1,13 @@
 /**
  * Basic code for assignment 2 - F2020
- * @author Sadeghi F , Safaa Bedawi
+ * @author Peiyu Lu
+ * id:150665940
  */
 
-package assignment2;
-
+//package luxx5940_a2;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class BaseDecisionTreeTester {
@@ -19,8 +22,7 @@ public class BaseDecisionTreeTester {
      *  read the train file
      */
 	
-	public static void main( String [] args ) {
-		
+	public static void main(String[] args ) {
     BaseDecisionTree.nClasses = 2;
     BaseDecisionTree.dim = 3;
     
@@ -28,6 +30,7 @@ public class BaseDecisionTreeTester {
      * is man?
      */
     BaseDecisionTree s1 = new BaseDecisionTree(0, 0.99);
+    //System.out.println(s1);
     BaseDecisionTree s2 = new BaseDecisionTree(); // leaf
     s2.setProb(1, 85); //  survived at 36% probability
 
@@ -37,15 +40,15 @@ public class BaseDecisionTreeTester {
      */
     BaseDecisionTree s3 = new BaseDecisionTree(1, 9.9);//
     BaseDecisionTree s4 = new BaseDecisionTree(); // leaf
-    s2.setProb(0, 61); //  not survived at  61%
+    s4.setProb(0, 61); //  not survived at  61%
     /*
      * is Siblings>2.5
      */
     BaseDecisionTree s5 = new BaseDecisionTree(2,2.9); // leaf
     BaseDecisionTree s6 = new BaseDecisionTree(); // leaf
-    s6.setProb(0, 15); // not survived at 2%
+    //s6.setProb(0, 15); // not survived at 2%
     BaseDecisionTree s7 = new BaseDecisionTree(); // leaf
-    s7.setProb(1, 25); // survived at 20%
+    //s7.setProb(1, 25); // survived at 20%
 
 
     s1.setSmallerBranch(s2);
@@ -60,23 +63,68 @@ public class BaseDecisionTreeTester {
      * the right answers if you want o check them with yours. "1" means survived and "0"
      * means not survived.
      */
+//    s1.CalcProb("input.txt");
+//    double[] test1 = {1,2,4};
+//    System.out.println("Man, 2 years old with 4 siblings = " + s1.getDecision(test1));//answer is:0
+//    double[] test2 = {1,10,5};
+//    System.out.println("Man 10 years old with 5 siblings = " + s1.getDecision(test2));//answer is:1
+//    double[] test3 = {0,2,0};
+//    System.out.println("Woman 2 years old = " + s1.getDecision(test3));//answer is 1
+//    double[] test4 = {1,20,0};
+//    System.out.println("Woman 40 years old = " + s1.getDecision(test4));//answer is:1
+//    double[] test5 = {1,34,1};
+//    System.out.println("Man, 10 years old with 1 siblings = " + s1.getDecision(test5));//answer is:0
+
+    String line;
+    String[] lineArr;
+    int tempClass;
+    int tempFeature;
+    BufferedReader reader;
+    try {
+        reader = new BufferedReader(new FileReader("input.txt"));
+        while((line = reader.readLine()) != null) {
+        	s1.CalcProb("input.txt");
+        	
+        }
+    }catch (IOException e) {
+        e.printStackTrace();
+    }
+    System.out.println(s1.CalcProb("input.txt")[0]);
+//
+    int[] classFreq = s3.CalcProb("gender");
+    System.out.println("survived: " + classFreq[1]);
+	System.out.println("not survived: " + classFreq[0] );
+	
+    int[] classFreq1 = s3.CalcProb("age");
+    System.out.println("survived: " + classFreq1[1]);
+	System.out.println("not survived: " + classFreq1[0]);
+	
+    int[] classFreq2 = s3.CalcProb("sibling");
+    System.out.println("survived: " + classFreq2[1]);
+	System.out.println("not survived: " + classFreq2[0]);
     
-    double[] test1 = {1,2,4};
-    System.out.println("Man, 2 years old with 4 siblings = " + s1.getDecision(test1));//answer is:0
-    double[] test2 = {1,10,5};
-    System.out.println("Man 10 years old with 5 siblings = " + s1.getDecision(test2));//answer is:1
-    double[] test3 = {0,2,0};
-    System.out.println("Woman 2 years old = " + s1.getDecision(test3));//answer is 1
-    double[] test4 = {1,20,0};
-    System.out.println("Woman 40 years old = " + s1.getDecision(test4));//answer is:1
-    double[] test5 = {1,34,1};
-    System.out.println("Man, 10 years old with 1 siblings = " + s1.getDecision(test5));//answer is:0
+    int[] result = s1.CalcAccuracy("output.txt");
+    System.out.println(result[0]);
     
+    int[] result1 = s6.CalcAccuracy("output.txt");
+    System.out.println(result1[0]);
+    
+    int[] result2 = s7.CalcAccuracy("output.txt");
+    System.out.println(result2[0]);
+//
+//    s1.print();
+    
+//    int[] classFreq4 = s1.CalcAccuracy("gender");
+//    System.out.println("survived: " + classFreq4[1]);
+//    int[] classFreq5 = s1.CalcAccuracy("age");
+//    System.out.println("survived: " + classFreq5[1]);
+//    int[] classFreq6 = s1.CalcAccuracy("sibling");
+//    System.out.println("survived: " + classFreq6[1]);
     /* Please comment out this part after you calculate the probabilities from 
      * input data. These inputs are based on output data and the answer for them is
      * in front of each one. you can test your decision tree with these.*/
 
-    /*
+    ///*
     double[] test1 = {0,39,1};
     System.out.println("Man, 2 years old with 4 siblings = " + s1.getDecision(test1));//answer is 1
     double[] test2 = {1,24,2};
@@ -87,7 +135,6 @@ public class BaseDecisionTreeTester {
     System.out.println("Woman 40 years old = " + s1.getDecision(test4));//answer is 0
     double[] test5 = {1, 10, 1};
     System.out.println("Man, 10 years old with 1 siblings = " + s1.getDecision(test5));//answer is 0
-     */
-   
+     //*/
 	}
 }
